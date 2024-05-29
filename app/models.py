d8e593974c7e363bd33a5e94ecdc21ef981d96fe
максим <maximovichpro@gmail.com>
2024-05-29 21:50:00 +0500

    add RepairWorker model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import uuid
 
 from app import db
-from schemas import RepairRequestStatus
+from .schemas import RepairRequestStatus
 
 
 class RepairRequest(db.Model):
@@ -16,6 +16,20 @@ class RepairRequest(db.Model):
     client_name = db.Column(db.String(100), nullable=False)
     client_phone = db.Column(db.String(20), nullable=False)
     status = db.Column(db.Enum(RepairRequestStatus), default=RepairRequestStatus.NEW, nullable=False)
+    current_master_id = db.Column(db.String, db.ForeignKey('repair_workers.id', name='fk_repair_requests_current_master_id'), nullable=True)
+    master_comment = db.Column(db.Text, nullable=True)
+
+    current_master = db.relationship('RepairWorker', back_populates='repair_requests')
 
     def __repr__(self):
         return f'<RepairRequest {self.id}>'
+
+
+class RepairWorker(db.Model):
+    __tablename__ = 'repair_workers'
+
+    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    repair_requests = db.relationship('RepairRequest', back_populates='current_master')
+
+    def __repr__(self):
+        return f'<RepairWorker {self.id}>'
--- app/schemas.py
+++ app/schemas.py
@@ -29,4 +29,4 @@ class RepairRequestResponse(BaseModel):
     status: RepairRequestStatus
 
     class Config:
-        orm_mode = True
+        from_attributes = True

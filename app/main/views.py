4adce85cb4d654feb4130ea9337b6283021b7706
максим <maximovichpro@gmail.com>
2024-05-30 20:44:12 +0500

    login page

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,6 @@ from flask import render_template
 
 def home():
     return render_template('home/home.html')
+
+def login():
+    return render_template('registration/login.html')
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,13 @@ from flask import request, render_template, jsonify, Blueprint
 from flask import request, jsonify
 from app.models import RepairRequest, RepairRequestStatus
 from app.schemas import RepairRequestCreate, RepairRequestResponse, RepairRequestUpdateStatus
-from .main.views import home
+from .main.views import home, login
 
 main = Blueprint('main', __name__)
 
 def register_routes(app, db):
     main.add_url_rule('/', 'home', home)
+    main.add_url_rule('/login', 'login', login)
     # main.add_url_rule('/register', 'register', home)
     # main.add_url_rule('/login', 'login', home)
 

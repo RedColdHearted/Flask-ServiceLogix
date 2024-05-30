7842b1ec92276fb547eafe97eab6dc73133c7cc8
максим <maximovichpro@gmail.com>
2024-05-30 21:52:04 +0500

    register and login views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,48 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.security import generate_password_hash
+
+from app.models import RepairWorker
+from app.forms import RegistrationForm, LoginForm
+from app import db
+
 
 def home():
     return render_template('home/home.html')
 
+
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        user = RepairWorker(username=form.username.data, email=form.email.data, password_hash=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('registration/register.html', title='Register', form=form)
+
+
 def login():
-    return render_template('registration/login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = RepairWorker.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash('Invalid username or password.')
+    return render_template('registration/login.html', form=form)
+
+
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))

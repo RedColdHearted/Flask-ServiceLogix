05865429e6655f24bda1c6bc7cd950ba61ab273d
максим <maximovichpro@gmail.com>
2024-06-02 00:04:48 +0500

    new manager and admin create cli

--- app/cli.py
+++ app/cli.py
@@ -1,17 +1,28 @@
 from werkzeug.security import generate_password_hash
 
-from app import db
-from app.models import Manager
+from app.models import User
 
 def register_cli(app, db):
-    @app.cli.command('create-manager')
+    @app.cli.command('create-admin')
     def create_admin():
+        """Создание админ пользователя"""
+        username = input('Введите имя пользователя: ')
+        email = input('Введите email: ')
+        hashed_password = generate_password_hash(input('Введите пароль: '))
+
+        admin = User(username=username, email=email, password_hash=hashed_password, is_admin=True)
+
+        db.session.add(admin)
+        db.session.commit()
+
+    @app.cli.command('create-manager')
+    def create_manager():
         """Создание менеджера"""
         username = input('Введите имя пользователя: ')
         email = input('Введите email: ')
         hashed_password = generate_password_hash(input('Введите пароль: '))
 
-        admin = Manager(username=username, email=email, password_hash=hashed_password, is_admin=True)
+        admin = User(username=username, email=email, password_hash=hashed_password, is_manager=True)
 
         db.session.add(admin)
         db.session.commit()

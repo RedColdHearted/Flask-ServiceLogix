61afbb36e8dc5d524df21a1cea6770d63b75b1de
максим <maximovichpro@gmail.com>
2024-06-05 00:34:09 +0500

    cli commands update

--- app/cli.py
+++ app/cli.py
@@ -2,15 +2,18 @@ from werkzeug.security import generate_password_hash
 
 from app.models import User
 
+def get_info():
+    username = input('Введите имя пользователя: ')
+    email = input('Введите email: ')
+    hashed_password = generate_password_hash(input('Введите пароль: '))
+    return username, email, hashed_password
+
 def register_cli(app, db):
     @app.cli.command('create-admin')
     def create_admin():
         """Создание админ пользователя"""
-        username = input('Введите имя пользователя: ')
-        email = input('Введите email: ')
-        hashed_password = generate_password_hash(input('Введите пароль: '))
-
-        admin = User(username=username, email=email, password_hash=hashed_password, is_admin=True)
+        username, email, hashed_password = get_info()
+        admin = User(username=username, email=email, password_hash=hashed_password, is_admin=True, template_name='admin-profile')
 
         db.session.add(admin)
         db.session.commit()
@@ -18,11 +21,17 @@ def register_cli(app, db):
     @app.cli.command('create-manager')
     def create_manager():
         """Создание менеджера"""
-        username = input('Введите имя пользователя: ')
-        email = input('Введите email: ')
-        hashed_password = generate_password_hash(input('Введите пароль: '))
+        username, email, hashed_password = get_info()
+        admin = User(username=username, email=email, password_hash=hashed_password, is_manager=True, template_name='manager-profile')
 
-        admin = User(username=username, email=email, password_hash=hashed_password, is_manager=True)
+        db.session.add(admin)
+        db.session.commit()
+
+    @app.cli.command('create-repairman')
+    def create_repairman():
+        """Создание ремонтника"""
+        username, email, hashed_password = get_info()
+        admin = User(username=username, email=email, password_hash=hashed_password, is_repairmain=True, template_name='repairman-profile')
 
         db.session.add(admin)
         db.session.commit()

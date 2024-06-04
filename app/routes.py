10ba39c1a15d2a69c50d8854c213b7879398a111
максим <maximovichpro@gmail.com>
2024-06-05 00:34:37 +0500

    profile route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .main.views import home, login, register, logout
+from .main.views import home, login, register, logout, profile
 
 main = Blueprint('main', __name__)
 
@@ -10,5 +10,6 @@ def register_routes(app, db):
     main.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
     main.add_url_rule('/logout', 'logout', logout, methods=['GET', 'POST'])
     main.add_url_rule('/register', 'register', register, methods=['GET', 'POST'])
+    main.add_url_rule('/profile', 'profile', profile)
 
     app.register_blueprint(main)

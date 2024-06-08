4dc930ed4a30b107824a17bace910f61c1134a82
максим <maximovichpro@gmail.com>
2024-06-08 18:43:55 +0500

    models update

--- app/models.py
+++ app/models.py
@@ -10,10 +10,10 @@ from app.schemas import RepairRequestStatus
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.String(100), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=True)
     is_manager = db.Column(db.Boolean, default=False, nullable=True)
     is_repairman = db.Column(db.Boolean, default=False, nullable=True)
@@ -47,7 +47,7 @@ class RepairRequest(db.Model):
     client_name = db.Column(db.String(100), nullable=False)
     client_phone = db.Column(db.String(20), nullable=False)
     status = db.Column(db.Enum(RepairRequestStatus), default=RepairRequestStatus.NEW, nullable=False)
-    current_master_id = db.Column(db.String, db.ForeignKey('users.id'), nullable=True)
+    current_master_id = db.Column(db.String(100), db.ForeignKey('users.id'), nullable=True)
     master_comment = db.Column(db.Text, nullable=True)
     is_active = db.Column(db.Boolean, default=True, nullable=True)
     complete_at = db.Column(db.DateTime, default=None, nullable=True)

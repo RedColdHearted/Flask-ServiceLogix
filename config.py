a250a06eceae424af8821f724c85412d6593fb6f
максим <maximovichpro@gmail.com>
2024-06-09 21:31:56 +0500

    views update

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 import os
+
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://chiefbiefkief:maxim2005@chiefbiefkief.mysql.pythonanywhere-services.com/chiefbiefkief$default')
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://chiefbiefkief:maxim2005@chiefbiefkief.mysql.pythonanywhere-services.com/chiefbiefkief$default')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     SEND_FILE_MAX_AGE_DEFAULT = 0  # Disable caching for static files
+
+
+class TestingConfig(Config):
+
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'  # Используем отдельную тестовую базу данных
+    WTF_CSRF_ENABLED = False  # Отключить CSRF для удобства тестирования

7e9066101b6526bc80780816303b755374d4d0e4
максим <maximovichpro@gmail.com>
2024-06-11 20:11:59 +0500

    config.py update

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://chiefbiefkief:maxim2005@chiefbiefkief.mysql.pythonanywhere-services.com/chiefbiefkief$default')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False

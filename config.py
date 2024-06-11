1b0fda5c564afbf4b0a0db46e208f9c11696a290
максим <maximovichpro@gmail.com>
2024-06-11 20:11:23 +0500

    config.py update

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+from dotenv import load_dotenv
+
 import os
 
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://chiefbiefkief:maxim2005@chiefbiefkief.mysql.pythonanywhere-services.com/chiefbiefkief$default')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     SEND_FILE_MAX_AGE_DEFAULT = 0  # Disable caching for static files

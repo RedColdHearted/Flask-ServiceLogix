d40d34a4a6af7cf8c0df3da5a5eebf61f7387fb1
максим <maximovichpro@gmail.com>
2024-06-12 15:04:34 +0500

    register view update

--- app/main/views.py
+++ app/main/views.py
@@ -34,7 +34,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password_hash=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password_hash=hashed_password, is_repairman=True)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')

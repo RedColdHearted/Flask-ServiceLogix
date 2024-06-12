61df0ccde73ff4dfac259621abdb44a8815fe015
максим <maximovichpro@gmail.com>
2024-06-12 13:18:55 +0500

    mpty commit to trigger itub ctions

--- config.py
+++ config.py
@@ -8,14 +8,12 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     SEND_FILE_MAX_AGE_DEFAULT = 0  # Disable caching for static files
 
 
 class TestingConfig(Config):
-
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'  # Используем отдельную тестовую базу данных
-    WTF_CSRF_ENABLED = False  # Отключить CSRF для удобства тестирования
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    WTF_CSRF_ENABLED = False
